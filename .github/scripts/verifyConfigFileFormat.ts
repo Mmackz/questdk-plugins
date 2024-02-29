072c84adab20f897dac13df0022400a916700e4f
mmackz <matthewmackenz@gmail.com>
2024-02-29 13:34:32 -0800

    target fork

--- .github/scripts/verifyConfigFileFormat.ts
+++ .github/scripts/verifyConfigFileFormat.ts
@@ -29,7 +29,7 @@ const PluginConfigSchema = z.object({
 
 async function getNewPackages(): Promise<string[]> {
   const { stdout, stderr } = await execAsync(
-    "git diff --diff-filter=A --name-only main...HEAD packages/",
+    "git diff --diff-filter=A --name-only mmackz/test...HEAD packages/",
   );
   if (stderr) {
     throw new Error(`Error getting new packages: ${stderr}`);
