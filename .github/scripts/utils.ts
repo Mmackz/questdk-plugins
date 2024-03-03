eb9139f91866aa2b77258712cb0e97a19edc5954
mmackz <62824345+mmackz@users.noreply.github.com>
2024-03-02 23:23:17 -0800

    Mmackz/testpr (#24)
    * add-builder
    
    * remove test
    
    * remove test
    
    * testpr
    
    * add new project bad url
    
    * change image url
    
    * invalid details
    
    * add text file
    
    * push
    
    * update getNewPackages
    
    * types
    
    * update
    
    * yoo
    
    * logging
    
    * check new files
    
    * log paths
    
    * logs
    
    * fix path
    
    * add valid plugin-details
    
    * remove log
    
    * update image
    
    * remove comments
    
    * try var
    
    * why
    
    * meh
    
    * remove test packages
    
    * test pr valid
    
    * add new details
    
    * changes
    
    * change
    
    * fix
    
    * compare test
    
    * changes
    
    * update existing details

--- .github/scripts/utils.ts
+++ .github/scripts/utils.ts
@@ -8,7 +8,7 @@ const execAsync = promisify(exec);
 async function getNewPackages(): Promise<string[]> {
   // Get list of all directories in packages/ on main
   const { stdout: mainDirs } = await execAsync(
-    "git ls-tree -d --name-only main:packages/",
+    "git ls-tree -d --name-only mmackz/test:packages/",
   );
   const mainPackagesSet = new Set(
     mainDirs.split("\n").filter((name: string) => name.trim() !== ""),
@@ -32,7 +32,7 @@ async function getNewPackages(): Promise<string[]> {
 
 async function getUpdatedPluginDetailsPaths(): Promise<string[]> {
   const { stdout, stderr } = await execAsync(
-    "git diff --diff-filter=AM --name-only main...HEAD packages/",
+    "git diff --diff-filter=AM --name-only mmackz/test...HEAD packages/",
   );
   if (stderr) {
     throw new Error(`Error getting updated plugin details: ${stderr}`);
