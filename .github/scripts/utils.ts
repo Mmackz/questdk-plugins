5ea4d4f8d9f272cfa66cd0ec73f0b1c582d53838
mmackz <matthewmackenz@gmail.com>
2024-03-03 08:42:26 -0800

    change

--- .github/scripts/utils.ts
+++ .github/scripts/utils.ts
@@ -47,23 +47,20 @@ async function getUpdatedPluginDetailsPaths(): Promise<string[]> {
   return updatedDetailsPaths;
 }
 
-async function getUpdatedPluginDetailsPathsNew(): Promise<string[]> {
-  // Assuming 'origin/mmackz/test' is the branch you're comparing against its immediate ancestor
+async function getPluginDetailsOnPushToMain(): Promise<string[]> {
   const { stdout, stderr } = await execAsync(
     "git diff --name-only HEAD^ HEAD -- 'packages/*plugin-details.yml'",
   );
   if (stderr) {
     throw new Error(`Error getting updated plugin details: ${stderr}`);
   }
-  console.log(stdout);
-  const updatedDetailsPaths = stdout
+  const detailsPaths = stdout
     .split("\n")
     .filter((path: string) => path.trim() !== "" && path.includes("plugin-details.yml"))
     .map((path: string) => path.replace("/plugin-details.yml", "").trim());
 
-  return updatedDetailsPaths;
+  return detailsPaths;
 }
-
 
 async function validatePluginDetailsPaths(
   newPackagesPaths: string[],
@@ -86,6 +83,6 @@ async function validatePluginDetailsPaths(
 module.exports = {
   getNewPackages,
   getUpdatedPluginDetailsPaths,
-  getUpdatedPluginDetailsPathsNew,
+  getPluginDetailsOnPushToMain,
   validatePluginDetailsPaths,
 };
