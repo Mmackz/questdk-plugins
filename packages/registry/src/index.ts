a4e54a901d094ff481ce35974b5ed17c243e7c95
mmackz <matthewmackenz@gmail.com>
2024-03-08 19:26:13 -0800

    remove appa

--- packages/registry/src/index.ts
+++ packages/registry/src/index.ts
@@ -31,7 +31,6 @@ import { Vela } from '@rabbitholegg/questdk-plugin-vela'
 import { WooFi } from '@rabbitholegg/questdk-plugin-woofi'
 import { Zora } from '@rabbitholegg/questdk-plugin-zora'
 import { JOJO } from '@rabbitholegg/questdk-plugin-jojo'
-import { Appa } from '@rabbitholegg/questdk-plugin-appa'
 import { ENTRYPOINT } from './contract-addresses'
 import {
   type IntentParams,
@@ -86,7 +85,6 @@ export const plugins: Record<string, IActionPlugin> = {
   [Llama.pluginId]: Llama,
   [Kote.pluginId]: Kote,
   [JOJO.pluginId]: JOJO,
-  [Appa.pluginId]: Appa,
 }
 
 export const getPlugin = (pluginId: string) => {
